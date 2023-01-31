1de5bf2701482d9ba642b0bdb7d45422a5ead48e
SalomeVeru <verulidzesalome23@gmail.com>
2023-01-31 21:54:58 +0400

    i've finished main page

---
+++ Socialbooks/src/script.js
@@ -0,0 +1,36 @@
+
+// ---------------- dropdown menu---------------
+
+var settingsmenu = document.querySelector(".settings-menu");
+var darkBtn = document.getElementById("dark-btn");
+
+function settingsMenuToggle(){
+    settingsmenu.classList.toggle("settings-menu-height");
+} 
+
+// ------------toggle ---------------
+darkBtn.onclick= function(){
+    darkBtn.classList.toggle("dark-btn-on");
+    document.body.classList.toggle("dark-theme")
+
+    if(localStorage.getItem("theme")== "light"){
+        localStorage.setItem("theme", "dark")
+    }
+    else {
+        localStorage.setItem("theme", "light");
+    }
+}
+
+// როცა საიტს დაარეფრეშებ, დარჩენს დარქმოუდი
+
+if (localStorage.getItem("theme")== "light"){
+    darkBtn.classList.remove("dark-btn-on");
+    document.body.classList.remove("dark-theme")
+}
+else if(localStorage.getItem("theme")== "dark"){
+    darkBtn.classList.add("dark-btn-on");
+    document.body.classList.add("dark-theme")
+}
+else {
+    localStorage.setItem("theme", "light");
+}
